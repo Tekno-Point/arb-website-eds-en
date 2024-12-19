5bf19bb8a9f7f2445f689ec2760c113acc53845b
pashtepooja18 <poojapashte18@gmail.com>
2024-12-19 16:00:08 +0530

    blog container component added and reset css added

--- blocks/categories/categories.js
+++ blocks/categories/categories.js
@@ -1,3 +1,3 @@
-export default function decorate(block){
-    block.innerHTML = '<h1>Categories</h1>';
+export default function decorate(block) {
+  block.innerHTML = '<h1>Categories</h1>';
 }
--- blocks/taglist/taglist.js
+++ blocks/taglist/taglist.js
@@ -1,3 +1,3 @@
-export default function decorate(block){
-    block.innerHTML = '<h1>Tags</h1>';
+export default function decorate(block) {
+  block.innerHTML = '<h1>Tags</h1>';
 }
