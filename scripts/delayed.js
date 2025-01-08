0e0e93324999eb0663415020471f1da1e06fc36a
pashtepooja18 <poojapashte18@gmail.com>
2025-01-08 15:13:50 +0530

    blog list, blog-details, categories css modified

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,1 +1,13 @@
 // add delayed functionality here
+function loadHeadGTM() {
+  const scriptTag = document.createElement('script');
+
+  scriptTag.innerHTML = `type="text/javascript" async=""
+src="https://static.klaviyo.com/onsite/js/PUBLIC_API_KEY/klaviyo.js"`;
+
+  document.head.prepend(scriptTag);
+}
+
+if (!window.location.hostname.includes('localhost') && !window.location.hostname.includes('author')) {
+  loadHeadGTM();
+}
